8e379551805ee7944f8be56cc0140ac43a5240a6
fieldforcemrser2026 <fieldforcemrser@gmail.com>
2026-02-06 17:37:03 +0100

    _workers.js

--- _workers.js
+++ _workers.js
@@ -2,29 +2,47 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     
-    // Se è una chiamata API, fai da proxy
     if (url.pathname === '/api') {
       const API_URL = 'https://script.google.com/macros/s/AKfycbwWke2ScBtrV4cjdMsx1f-diEabp2v_rEUTuTBE5zKv4wfxcvXpTfiO5A3Q6q_lIJC8Kg/exec';
       
-      const targetUrl = API_URL + url.search;
+      let targetUrl = API_URL + url.search;
+      let response;
       
-      const response = await fetch(targetUrl, {
-        method: request.method,
-        headers: request.headers,
-        body: request.method !== 'GET' ? await request.text() : null
-      });
+      if (request.method === 'POST') {
+        const body = await request.text();
+        response = await fetch(targetUrl, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: body,
+          redirect: 'follow'
+        });
+      } else {
+        response = await fetch(targetUrl, { redirect: 'follow' });
+      }
       
       const data = await response.text();
       
       return new Response(data, {
+        status: 200,
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type'
         }
       });
     }
     
-    // Altrimenti servi i file statici
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type'
+        }
+      });
+    }
+    
     return env.ASSETS.fetch(request);
   }
 };
